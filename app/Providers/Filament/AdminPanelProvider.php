83e540268681b878e160c94336eaa7ac6ce03882
An030905 <nguyentrongan20055@gmail.com>
2026-04-05 11:13:02 +0700

    style: Tuy chinh mau sac giao dien va thay doi Icon menu

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -29,7 +29,7 @@ class AdminPanelProvider extends PanelProvider
             ->path('sv23810310263-admin')
             ->login()
             ->colors([
-                'primary' => Color::Amber,
+                'primary' => \Filament\Support\Colors\Color::Sky,
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
